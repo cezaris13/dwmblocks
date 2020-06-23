f043ab56f3ca3e694af162d3c79f63266040d0f5
cezaris13 <pijus.cezar13@gmail.com>
2020-06-23 16:19:12 +0300

    rename

--- config.h
+++ config.h
@@ -3,9 +3,9 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
     {"","$HOME/.config/dwm/scripts/./bar.sh lock_keys",2,13},
     {"","$HOME/.config/dwm/scripts/./bar.sh layout",0,12},
-    {" ","$HOME/.config/dwm/scripts/./bar.sh vol",0,10},
-    {" ","$HOME/.config/dwm/scripts/./bar.sh net",5,4},
-    {"", "$HOME/.config/dwm/scripts/./bar.sh bat",5,3},
+    {" ","$HOME/.config/dwm/scripts/./bar.sh volume",0,10},
+    {" ","$HOME/.config/dwm/scripts/./bar.sh network",5,4},
+    {"", "$HOME/.config/dwm/scripts/./bar.sh battery",5,3},
     {" ","$HOME/.config/dwm/scripts/./bar.sh date",1,21},
 };
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
