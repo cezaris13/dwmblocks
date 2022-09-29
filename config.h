c5a0eea874f0fa59126248bf877d90fbe0b3738b
cezaris13 <pijus.cezar13@gmail.com>
2022-09-29 20:58:22 +0300

    organizing scripts

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {"","$HOME/.config/dwm/scripts/./bar.sh lock_keys",2,13},
-    {"","$HOME/.config/dwm/scripts/./bar.sh layout",0,12},
-    {" ","$HOME/.config/dwm/scripts/./bar.sh volume",0,10},
-    {" ","$HOME/.config/dwm/scripts/./bar.sh network",5,4},
-    {"", "$HOME/.config/dwm/scripts/./bar.sh battery",5,3},
-    {" ","$HOME/.config/dwm/scripts/./bar.sh date",1,21},
+    {"","$HOME/.config/dwm/scripts/dwm_specific/./bar.sh lock_keys",2,13},
+    {"","$HOME/.config/dwm/scripts/dwm_specific/./bar.sh layout",0,12},
+    {" ","$HOME/.config/dwm/scripts/dwm_specific/./bar.sh volume",0,10},
+    {" ","$HOME/.config/dwm/scripts/dwm_specific/./bar.sh network",5,4},
+    {"", "$HOME/.config/dwm/scripts/dwm_specific/./bar.sh battery",5,3},
+    {" ","$HOME/.config/dwm/scripts/dwm_specific/./bar.sh date",1,21},
 };
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char *delim = "|";
